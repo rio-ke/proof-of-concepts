9b44abdf0d7c9d50416c9a2492051a2279c439b9
fourtimes <jino@gmail.com>
2022-06-27 21:58:26 +0530

    conditional statement added

--- module/aws-subnet/test/test.tf
+++ module/aws-subnet/test/test.tf
@@ -8,20 +8,12 @@ module "as" {
   vpc_id = "vpc-04fc3b6b0abc327a9"
   tags = {
     "map-migrated" = "d-server-00xwtj6vmucwr9"
-    "name"         = "demo"
   }
   subnets = {
     GWM-APP-AZ1 = {
-      assign_ipv6_address_on_creation                = false
-      availability_zone                              = "ap-southeast-1a"
-      availability_zone_id                           = "apse1-az1"
-      cidr_block                                     = "10.60.82.128/26"
-      enable_dns64                                   = false
-      enable_resource_name_dns_a_record_on_launch    = false
-      enable_resource_name_dns_aaaa_record_on_launch = false
-      ipv6_native                                    = false
-      map_customer_owned_ip_on_launch                = false
-      map_public_ip_on_launch                        = false
+      availability_zone    = "ap-southeast-1a"
+      availability_zone_id = "apse1-az1"
+      cidr_block           = "10.60.82.128/26"
       tags = {
         "map-migrated" = "d-server-00xwtj6vmucwr9"
       }
