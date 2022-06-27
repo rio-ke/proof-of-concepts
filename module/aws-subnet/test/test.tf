5357f89c3a94ab4fe122205f707e697de77f7078
fourtimes <jino@gmail.com>
2022-06-27 22:54:40 +0530

    conditional statement added

--- module/aws-subnet/test/test.tf
+++ module/aws-subnet/test/test.tf
@@ -4,25 +4,23 @@ provider "aws" {
 
 
 module "as" {
-  source = "../"
-  vpc_id = "vpc-04fc3b6b0abc327a9"
+  source            = "../"
+  vpc_id            = "vpc-04fc3b6b0abc327a9"
+  availability_zone = "ap-southeast-1a"
   tags = {
     "map-migrated" = "d-server-00xwtj6vmucwr9"
   }
   subnets = {
     GWM-APP-AZ1 = {
-      availability_zone    = "ap-southeast-1a"
-      availability_zone_id = "apse1-az1"
-      cidr_block           = "10.60.82.128/26"
+      cidr_block = "10.60.82.128/26"
       tags = {
         "map-migrated" = "d-server-00xwtj6vmucwr9"
       }
     },
     GWM-APP-AZ2 = {
-      vpc_id               = "vpc-04fc3b6b0abc327a9"
-      availability_zone    = "ap-southeast-1a"
-      availability_zone_id = "apse1-az1"
-      cidr_block           = "10.60.82.128/26"
+      vpc_id            = "vpc-04fc3b6b0abc327a9"
+      availability_zone = "ap-southeast-1a"
+      cidr_block        = "10.60.82.128/26"
     }
   }
 }
