35d2e1b442ef85a1323a01ba98ce276ae780a4c8
cloud <cloud@instance.com>
2022-10-17 11:27:46 +0530

    added

--- terraform/event-driven/stageOne.tf
+++ terraform/event-driven/stageOne.tf
@@ -77,9 +77,33 @@ resource "aws_s3_bucket_notification" "s1" {
   }
 }
 
-resource "aws_sqs_queue" "terraform_queue" {
+resource "aws_sqs_queue" "s1" {
   name                  = "a3-sqs-queue.fifo"
   fifo_queue            = true
   deduplication_scope   = "messageGroup"
   fifo_throughput_limit = "perMessageGroupId"
 }
+resource "aws_sqs_queue_policy" "s1" {
+  queue_url = aws_sqs_queue.s1.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Id" : "sqspolicy",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "sns.amazonaws.com"
+        },
+        "Action" : "SQS:SendMessage",
+        "Resource" : "${aws_sqs_queue.s1.arn}",
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "${aws_sns_topic.s1.arn}"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
