099e10647df25a227787434d0c372f6857b67a68
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-18 17:35:40 +0530

    Update stageOne.tf

--- terraform/event-driven/stageOne.tf
+++ terraform/event-driven/stageOne.tf
@@ -2,14 +2,14 @@ resource "aws_s3_bucket" "s1" {
   bucket = "dodo-a1-bucket-s3"
 }
 
-# resource "aws_s3_bucket_notification" "s1" {
-#   bucket = aws_s3_bucket.s1.id
-#   lambda_function {
-#     lambda_function_arn = aws_lambda_function.s1.arn
-#     events              = ["s3:ObjectCreated:*"]
+resource "aws_s3_bucket_notification" "s1" {
+  bucket = aws_s3_bucket.s1.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s1.arn
+    events              = ["s3:ObjectCreated:*"]
 #     id                  = "dodo-a2-s3-to-lambda-notification"
-#   }
-# }
+  }
+}
 
 resource "aws_s3_bucket" "s22" {
   bucket = "dodo-b1-bucket-s3"
