49c46d2c2b9ed4cf49727347dc733c3872a46580
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-18 10:55:13 +0530

    Update stageOne.tf

--- terraform/event-driven/stageOne.tf
+++ terraform/event-driven/stageOne.tf
@@ -10,14 +10,14 @@ resource "aws_lambda_layer_version" "l1" {
   filename   = "${path.module}/layer/boto3.zip"
   layer_name = "currentBoto3"
   compatible_runtimes = ["python3.9"]
-  compatible_architectures =  "x86_64"
+  compatible_architectures =  ["x86_64"]
 }
 
 resource "aws_lambda_layer_version" "l2" {
   filename   = "${path.module}/layer/request.zip"
   layer_name = "currentRequest"
   compatible_runtimes = ["python3.9"]
-  compatible_architectures =  "x86_64"
+  compatible_architectures =  ["x86_64"]
 }
 
 data "archive_file" "s1" {
