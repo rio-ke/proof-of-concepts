48eebf45160b9c508ba8884cb3929b9a1b007ef8
fourtimes <jjino@protonmail.com>
2022-10-20 21:13:20 +0530

    seperate

--- terraform/event-driven/s3-bucket-s1.tf
+++ terraform/event-driven/s3-bucket-s1.tf
@@ -2,12 +2,12 @@ resource "aws_s3_bucket" "a1" {
   bucket = "op-a1-bucket-s3"
 }
 
-# resource "aws_s3_bucket_notification" "a1" {
-#   bucket = aws_s3_bucket.a1.id
-#   lambda_function {
-#     lambda_function_arn = aws_lambda_function.a2.arn
-#     events              = ["s3:ObjectCreated:*"]
-#     id                  = "op-s3-to-lambda-notification"
-#   }
-# }
+resource "aws_s3_bucket_notification" "a1" {
+  bucket = aws_s3_bucket.a1.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.a2.arn
+    events              = ["s3:ObjectCreated:*"]
+    # id                  = "op-s3-to-lambda-notification"
+  }
+}
 
