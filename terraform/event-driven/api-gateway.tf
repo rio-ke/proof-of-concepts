13f4adfd5d1e86aac74fcb19158538af716b51d3
fourtimes <jjino@protonmail.com>
2022-10-20 22:26:36 +0530

    seperate

--- terraform/event-driven/api-gateway.tf
+++ terraform/event-driven/api-gateway.tf
@@ -5,3 +5,16 @@ resource "aws_api_gateway_rest_api" "api" {
     types = ["REGIONAL"]
   }
 }
+
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "data"
+}
+
+resource "aws_api_gateway_method" "get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
