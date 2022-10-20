795cdc24fbbd2c09d04c79db83da808fe494ec76
fourtimes <jjino@protonmail.com>
2022-10-20 21:11:12 +0530

    seperate

--- terraform/event-driven/s3-bucket-s2.tf
+++ terraform/event-driven/s3-bucket-s2.tf
@@ -2,11 +2,11 @@ resource "aws_s3_bucket" "b1" {
   bucket = "op-b1-bucket-s3"
 }
 
-resource "aws_s3_bucket_notification" "b1" {
-  bucket = aws_s3_bucket.b1.id
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.b2.arn
-    events              = ["s3:ObjectTagging:*"]
-    id                  = "op-s3-to-lambda-tag-notification"
-  }
-}
+# resource "aws_s3_bucket_notification" "b1" {
+#   bucket = aws_s3_bucket.b1.id
+#   lambda_function {
+#     lambda_function_arn = aws_lambda_function.b2.arn
+#     events              = ["s3:ObjectTagging:*"]
+#     id                  = "op-s3-to-lambda-tag-notification"
+#   }
+# }
--- terraform/event-driven/s3-bucket-s3.tf
+++ terraform/event-driven/s3-bucket-s3.tf
@@ -3,11 +3,11 @@ resource "aws_s3_bucket" "c1" {
   bucket = "op-abc1-bucket-s3"
 }
 
-resource "aws_s3_bucket_notification" "c1" {
-  bucket = aws_s3_bucket.c1.id
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.c2.arn
-    events              = ["s3:ObjectTagging:*"]
-    id                  = "op-s3-to-lambda-tag-notification"
-  }
-}
+# resource "aws_s3_bucket_notification" "c1" {
+#   bucket = aws_s3_bucket.c1.id
+#   lambda_function {
+#     lambda_function_arn = aws_lambda_function.c2.arn
+#     events              = ["s3:ObjectTagging:*"]
+#     id                  = "op-s3-to-lambda-tag-notification"
+#   }
+# }
