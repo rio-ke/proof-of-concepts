71414532105f12b59f1c36bffd4f704af645d1fe
fourtimes <jjino@protonmail.com>
2022-10-20 19:57:51 +0530

    tf variable changes

--- terraform/event-driven/sns-topic.tf
+++ terraform/event-driven/sns-topic.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "common" {
+data "aws_iam_policy_document" "a3" {
   policy_id = "__default_policy__"
   statement {
     actions = [
@@ -23,7 +23,7 @@ data "aws_iam_policy_document" "common" {
       type        = "AWS"
       identifiers = ["*"]
     }
-    resources = [aws_sns_topic.s1.arn, aws_sns_topic.s2.arn, aws_sns_topic.s3.arn]
+    resources = [aws_sns_topic.s1.arn]
     sid       = "__sub_and_pub__"
   }
 }
@@ -44,12 +44,41 @@ resource "aws_sns_topic_subscription" "s1" {
 
 resource "aws_sns_topic_policy" "s1" {
   arn    = aws_sns_topic.s1.arn
-  policy = data.aws_iam_policy_document.common.json
+  policy = data.aws_iam_policy_document.a2.json
 }
 
 
 ## second stage
 
+data "aws_iam_policy_document" "b3" {
+  policy_id = "__default_policy__"
+  statement {
+    actions = [
+      "SNS:Publish",
+      "SNS:RemovePermission",
+      "SNS:SetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:AddPermission",
+      "SNS:Subscribe"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+      values = [
+        data.aws_caller_identity.account.account_id
+      ]
+    }
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = [aws_sns_topic.s2.arn]
+    sid       = "__sub_and_pub__"
+  }
+}
 resource "aws_sns_topic" "s2" {
   name                        = "b3-sns-topic.fifo"
   fifo_topic                  = true
@@ -58,7 +87,7 @@ resource "aws_sns_topic" "s2" {
 
 resource "aws_sns_topic_policy" "s2" {
   arn    = aws_sns_topic.s2.arn
-  policy = data.aws_iam_policy_document.common.json
+  policy = data.aws_iam_policy_document.b3.json
 }
 
 resource "aws_sns_topic_subscription" "s2" {
@@ -69,6 +98,35 @@ resource "aws_sns_topic_subscription" "s2" {
 
 ## Third stage
 
+data "aws_iam_policy_document" "c3" {
+  policy_id = "__default_policy__"
+  statement {
+    actions = [
+      "SNS:Publish",
+      "SNS:RemovePermission",
+      "SNS:SetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:AddPermission",
+      "SNS:Subscribe"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+      values = [
+        data.aws_caller_identity.account.account_id
+      ]
+    }
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = [aws_sns_topic.s3.arn]
+    sid       = "__sub_and_pub__"
+  }
+}
 resource "aws_sns_topic" "s3" {
   name                        = "c3-sns-topic.fifo"
   fifo_topic                  = true
@@ -77,7 +135,7 @@ resource "aws_sns_topic" "s3" {
 
 resource "aws_sns_topic_policy" "s3" {
   arn    = aws_sns_topic.s3.arn
-  policy = data.aws_iam_policy_document.common.json
+  policy = data.aws_iam_policy_document.c3.json
 }
 
 resource "aws_sns_topic_subscription" "s3" {
