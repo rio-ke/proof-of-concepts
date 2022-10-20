09bc4fb33ca41fb2b38f426882f59af6ddce3266
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-20 10:26:24 +0530

    Update and rename stageOne.tf to lambda.tf

--- terraform/event-driven/lambda.tf
+++ terraform/event-driven/lambda.tf
@@ -4,42 +4,6 @@ data "archive_file" "s1" {
   output_path = "${path.module}/stageOne/a2.py.zip"
 }
 
-resource "aws_iam_role" "s1" {
-  name = "stage-a2-lambda-lambda-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "s1" {
-  name = "stage-a2-lambda-lambda-role-policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = ["*"]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "s1" {
-  role       = aws_iam_role.s1.name
-  policy_arn = aws_iam_policy.s1.arn
-}
-
 resource "aws_lambda_function" "s1" {
   filename      = "${path.module}/stageOne/a2.py.zip"
   function_name = "stage-a2-lambda"
