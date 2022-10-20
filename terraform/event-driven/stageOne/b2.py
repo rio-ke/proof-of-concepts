a0efcb9a1beb00c3aa2922c64b2961c303983c30
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-20 10:46:47 +0530

    Update b2.py

--- terraform/event-driven/stageOne/b2.py
+++ terraform/event-driven/stageOne/b2.py
@@ -1,1 +1,11 @@
-# print("b2 lambda")_
+import json
+import boto3
+
+snsClient = boto3.client('sns')
+
+
+snsArn = tagvalue = os.environ['snsArn']  # snsArn="arn:aws:sns:ap-south-1:653413855845:b3-sns.fifo"
+
+def lambda_handler(event, context):
+    response = snsClient.publish(MessageGroupId="stageTwo",TargetArn=snsArn, Message=json.dumps({'default': json.dumps(event)}), MessageStructure='json')
+    print(f' <= event published to sns')
