0b325de748c9a00afcdf06853110cc77daf05542
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-20 11:21:50 +0530

    Update a2.py

--- terraform/event-driven/stageOne/a2.py
+++ terraform/event-driven/stageOne/a2.py
@@ -4,7 +4,7 @@ import os
 
 snsClient = boto3.client('sns')
 
-snsArn = tagvalue = os.environ['snsArn']  # "arn:aws:sns:ap-south-1:653413855845:a3-sns.fifo"
+snsArn = os.environ['snsArn']  # "arn:aws:sns:ap-south-1:653413855845:a3-sns.fifo"
 
 def lambda_handler(event, context):
     modifiedEvents = event['Records']
