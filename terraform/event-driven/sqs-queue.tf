ee3b14ecdc7720a81101619e77a0a8c54d7c1d77
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-20 10:43:18 +0530

    Update sqs-queue.tf

--- terraform/event-driven/sqs-queue.tf
+++ terraform/event-driven/sqs-queue.tf
@@ -28,3 +28,69 @@ resource "aws_sqs_queue_policy" "s1" {
     ]
   })
 }
+
+## second stage
+
+resource "aws_sqs_queue" "s2" {
+  name                        = "b4-sqs-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+  deduplication_scope         = "messageGroup"
+  fifo_throughput_limit       = "perQueue"
+}
+
+resource "aws_sqs_queue_policy" "s2" {
+  queue_url = aws_sqs_queue.s2.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Id" : "sqspolicy",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "sns.amazonaws.com"
+        },
+        "Action" : "SQS:SendMessage",
+        "Resource" : "${aws_sqs_queue.s2.arn}",
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "${aws_sns_topic.s2.arn}"
+          }
+        }
+      }
+    ]
+  })
+}
+
+## third stage
+
+resource "aws_sqs_queue" "s3" {
+  name                        = "c4-sqs-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+  deduplication_scope         = "messageGroup"
+  fifo_throughput_limit       = "perQueue"
+}
+
+resource "aws_sqs_queue_policy" "s3" {
+  queue_url = aws_sqs_queue.s3.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Id" : "sqspolicy",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "sns.amazonaws.com"
+        },
+        "Action" : "SQS:SendMessage",
+        "Resource" : "${aws_sqs_queue.s3.arn}",
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "${aws_sns_topic.s3.arn}"
+          }
+        }
+      }
+    ]
+  })
+}
