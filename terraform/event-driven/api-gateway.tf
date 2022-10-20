23fd8b79058803a15bf8702fb4326c989f2fad4a
fourtimes <jjino@protonmail.com>
2022-10-20 23:28:45 +0530

    fmt

--- terraform/event-driven/api-gateway.tf
+++ terraform/event-driven/api-gateway.tf
@@ -1,27 +1,27 @@
 resource "aws_api_gateway_rest_api" "api" {
   name        = "op-api"
-  description = "This API for op purposes"
+  description = "This api to call sqs messages"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "root" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = ""
-}
+# resource "aws_api_gateway_resource" "root" {
+#   rest_api_id = aws_api_gateway_rest_api.api.id
+#   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+#   path_part   = ""
+# }
 
 resource "aws_api_gateway_method" "get" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.root.id
+  resource_id   = aws_api_gateway_rest_api.api.root_resource_id # aws_api_gateway_resource.root.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "get" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.root.id
+  resource_id             = aws_api_gateway_rest_api.api.root_resource_id # aws_api_gateway_resource.root.id
   http_method             = aws_api_gateway_method.get.http_method
   type                    = "AWS"
   integration_http_method = "GET"
@@ -34,7 +34,7 @@ resource "aws_api_gateway_integration" "get" {
 
 resource "aws_api_gateway_method_response" "get" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.root.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id # aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.get.http_method
   status_code = "200"
   response_models = {
@@ -44,7 +44,7 @@ resource "aws_api_gateway_method_response" "get" {
 
 resource "aws_api_gateway_integration_response" "get" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.root.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id # aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.get.http_method
   status_code = aws_api_gateway_method_response.get.status_code
 }
