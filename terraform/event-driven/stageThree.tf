fd4949440de15883866caab514e38ea6a7b5072b
fourtimes <jjino@protonmail.com>
2022-10-20 19:52:47 +0530

    unwanted variable removed

--- terraform/event-driven/stageThree.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "s3" {
-  bucket = "stage-abc1-bucket-s3"
-  tags = {
-    Name        = "abc1-bucket-s3"
-    Environment = "development"
-  }
-}
--- terraform/event-driven/stateTwo.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "s2" {
-  bucket = "stage-b1-bucket-s3"
-  tags = {
-    Name        = "b1-bucket-s3"
-    Environment = "development"
-  }
-}
