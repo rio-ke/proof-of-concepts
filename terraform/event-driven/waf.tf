74d0566e19e1e93ca8ad8a3abf82a1c0b97a9f17
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-21 19:59:12 +0530

    Update waf.tf

--- terraform/event-driven/waf.tf
+++ terraform/event-driven/waf.tf
@@ -4,11 +4,11 @@ resource "aws_wafv2_web_acl" "waf" {
   default_action {
     allow {}
   }
-#   visibility_config {
-#     cloudwatch_metrics_enabled = false
-#     metric_name                = "friendly-metric-name"
-#     sampled_requests_enabled   = false
-#   }
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "${var.apigateway}-waf-metric"
+    sampled_requests_enabled   = false
+  }
 }
 
 # resource "aws_wafv2_web_acl_association" "waf" {
