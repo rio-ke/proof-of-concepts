0575960c6b1432d16d59241e18c9089c6dbad5af
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-21 19:22:52 +0530

    Update variable.tf

--- terraform/event-driven/variable.tf
+++ terraform/event-driven/variable.tf
@@ -3,3 +3,7 @@ variable "alertSqsQueueURL" {}
 variable "stageOneBucket" {}
 variable "stageTwoBucket" {}
 variable "stageThreeBucket" {}
+
+variable "stageTwoSns" {}
+variable "stageTwoSns" {}
+variable "stageThreeSns" {}
