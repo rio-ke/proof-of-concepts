e364e8ccc7ac1e2a1ab509ed61ab94cfd4ab9a6c
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-22 11:37:05 +0530

    Update api-gateway.tf

--- terraform/event-driven/api-gateway.tf
+++ terraform/event-driven/api-gateway.tf
@@ -49,16 +49,18 @@ resource "aws_api_gateway_integration_response" "get" {
   status_code = aws_api_gateway_method_response.get.status_code
 }
 
-resource "aws_api_gateway_deployment" "get" {
-  depends_on  = [aws_api_gateway_integration.get]
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "queue"
-}
+
 
 resource "aws_api_gateway_stage" "get" {
   deployment_id = aws_api_gateway_deployment.get.id
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  stage_name    = "queue"
+  stage_name    = "sqsQueueReader"
+}
+
+resource "aws_api_gateway_deployment" "get" {
+  depends_on  = [aws_api_gateway_integration.get, aws_api_gateway_stage.get]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "sqsQueueReader"
 }
 
 resource "aws_wafv2_web_acl_association" "waf" {
