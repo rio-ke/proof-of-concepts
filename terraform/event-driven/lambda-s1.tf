bef03febeee6a1e67a94e5ecbc00c478493a8403
fourtimes <57703276+FourTimes@users.noreply.github.com>
2022-10-22 17:00:17 +0530

    Update lambda-s1.tf

--- terraform/event-driven/lambda-s1.tf
+++ terraform/event-driven/lambda-s1.tf
@@ -33,9 +33,52 @@ resource "aws_lambda_function" "a5" {
   layers        = [aws_lambda_layer_version.l1.arn, aws_lambda_layer_version.l2.arn]
   environment {
     variables = {
-      destination_bucket_name             = aws_s3_bucket.c1.bucket # abc1-bucket-s3
-      replication_destination_bucket_name = aws_s3_bucket.b1.bucket # "b1-bucket-s3"
-      sqsUrl                              = aws_sqs_queue.a4.url
+data "archive_file" "a2" {
+  type        = "zip"
+  source_file = "${path.module}/lambdaHandlers/a2.py"
+  output_path = "${path.module}/lambdaHandlers/a2.py.zip"
+}
+
+resource "aws_lambda_function" "a2" {
+  filename      = "${path.module}/lambdaHandlers/a2.py.zip"
+  function_name = "op-a2-lambda"
+  role          = aws_iam_role.common.arn
+  handler       = "a2.lambda_handler"
+  runtime       = "python3.9"
+  layers        = [aws_lambda_layer_version.l1.arn, aws_lambda_layer_version.l2.arn]
+  environment {
+    variables = {
+      snsArn = aws_sns_topic.a3.arn
+    }
+  }
+}
+
+data "archive_file" "a5" {
+  type        = "zip"
+  source_file = "${path.module}/lambdaHandlers/a5.py"
+  output_path = "${path.module}/lambdaHandlers/a5.py.zip"
+}
+
+resource "aws_lambda_function" "a5" {
+  filename      = "${path.module}/lambdaHandlers/a5.py.zip"
+  function_name = "op-a5-lambda"
+  role          = aws_iam_role.common.arn
+  handler       = "a5.lambda_handler"
+  runtime       = "python3.9"
+  layers        = [aws_lambda_layer_version.l1.arn, aws_lambda_layer_version.l2.arn]
+  environment {
+    variables = {
+      metadataBucket = aws_s3_bucket.c1.bucket 
+      scanningBucket = aws_s3_bucket.b1.bucket
+      sqsUrl         = aws_sqs_queue.a4.url
+    }
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "a5" {
+  event_source_arn = aws_sqs_queue.a4.arn
+  function_name    = aws_lambda_function.a5.arn
+}
     }
   }
 }
