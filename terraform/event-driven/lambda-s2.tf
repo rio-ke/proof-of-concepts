1deb9505c6b4df40533a99638f793a714a7955b8
fourtimes <jjino@protonmail.com>
2022-10-24 10:06:24 +0530

    a2 lambda role restriction added

--- terraform/event-driven/lambda-s2.tf
+++ terraform/event-driven/lambda-s2.tf
@@ -1,3 +1,49 @@
+resource "aws_iam_role" "b2" {
+  name = "${var.stageTwoLambdaOne}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "b2" {
+  name = "${var.stageTwoLambdaOne}-role-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "logs:CreateLogGroup",
+        Resource = "arn:aws:logs:${var.region}:${data.aws_caller_identity.account.account_id}:*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["logs:CreateLogStream", "logs:PutLogEvents"],
+        Resource = ["arn:aws:logs:${var.region}:${data.aws_caller_identity.account.account_id}:log-group:/aws/lambda/${var.stageTwoLambdaOne}:*"]
+      },
+      {
+        Action   = ["sns:Publish"]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "b2" {
+  role       = aws_iam_role.a2.name
+  policy_arn = aws_iam_policy.a2.arn
+}
+
 
 data "archive_file" "b2" {
   type        = "zip"
@@ -9,7 +55,7 @@ resource "aws_lambda_function" "b2" {
   filename         = data.archive_file.b2.output_path
   source_code_hash = data.archive_file.b2.output_base64sha256
   function_name    = var.stageTwoLambdaOne
-  role             = aws_iam_role.common.arn
+  role             = aws_iam_role.b2.arn
   handler          = "b2.lambda_handler"
   runtime          = "python3.9"
   layers           = [aws_lambda_layer_version.l1.arn, aws_lambda_layer_version.l2.arn]
