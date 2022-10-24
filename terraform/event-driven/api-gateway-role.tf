29924889f53ae5db3c9e350814b6a1e89923542d
fourtimes <jjino@protonmail.com>
2022-10-24 11:39:18 +0530

    added

--- terraform/event-driven/api-gateway-role.tf
+++ terraform/event-driven/api-gateway-role.tf
@@ -21,9 +21,35 @@ resource "aws_iam_policy" "apigateway" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action   = ["*"]
-        Effect   = "Allow"
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams",
+          "logs:PutLogEvents",
+          "logs:GetLogEvents",
+          "logs:FilterLogEvents"
+        ],
         Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "sqs:GetQueueUrl",
+          "sqs:ChangeMessageVisibility",
+          "sqs:ListDeadLetterSourceQueues",
+          "sqs:SendMessageBatch",
+          "sqs:PurgeQueue",
+          "sqs:ReceiveMessage",
+          "sqs:SendMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:CreateQueue",
+          "sqs:ListQueueTags",
+          "sqs:ChangeMessageVisibilityBatch",
+          "sqs:SetQueueAttributes"
+        ],
+        Resource = "${aws_sqs_queue.c4.arn}"
       }
     ]
   })
