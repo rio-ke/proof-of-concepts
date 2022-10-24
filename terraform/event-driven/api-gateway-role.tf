cfaa9e48527c8193491e5d18e7ffb3e4c45a857e
fourtimes <jjino@protonmail.com>
2022-10-24 11:49:09 +0530

    restrict apigateway iam

--- terraform/event-driven/api-gateway-role.tf
+++ terraform/event-driven/api-gateway-role.tf
@@ -23,19 +23,6 @@ resource "aws_iam_policy" "apigateway" {
       {
         Effect = "Allow",
         Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams",
-          "logs:PutLogEvents",
-          "logs:GetLogEvents",
-          "logs:FilterLogEvents"
-        ],
-        Resource = "*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
           "sqs:GetQueueUrl",
           "sqs:ChangeMessageVisibility",
           "sqs:ListDeadLetterSourceQueues",
