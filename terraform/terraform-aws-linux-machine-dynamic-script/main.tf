9c6ac9e4ebaff59215e164270dcfcb0e4bfbc0e6
operation-unknown <57703276+fourtimes@users.noreply.github.com>
2022-11-08 14:18:04 +0530

    Add files via upload

---
+++ terraform/terraform-aws-linux-machine-dynamic-script/main.tf
@@ -0,0 +1,22 @@
+data "template_file" "scriptData" {
+  template = file("scripts/main.sh")
+  vars = {
+    efsvol = var.ssmParameterName
+  }
+}
+
+
+resource "aws_instance" "neo-server" {
+  ami                    = var.amis[var.region]
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.neo-security-group.id]
+  key_name               = var.key-name
+  user_data              = data.template_file.scriptData.rendered
+  tags                   = merge(map("Name", "${var.project}-neo-server"), var.additional_tags)
+
+}
+
+variable "ssmParameterName" {
+    default = "ubuntu"
+}
---
+++ terraform/terraform-aws-linux-machine-dynamic-script/scripts/main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ex
+echo "SSM PARAMS NAME IS => ${ssmParameterName}"
+sudo apt update
+sudo apt install wget -y
+cd /tmp
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl start amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i amazon-cloudwatch-agent.deb
+sudo mkdir -p /usr/share/collectd/
+sudo touch /usr/share/collectd/types.db
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c ssm:${ssmParameterName}
+sudo systemctl start amazon-cloudwatch-agent
+sudo systemctl enable amazon-cloudwatch-agent
